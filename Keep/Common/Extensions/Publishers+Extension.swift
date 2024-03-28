1e0383275582c860a843fdbcc1d7587077775745
Myung Hoon So <joa214@hotmail.com>
2024-03-28 12:03:55 +0000

    Publishers extension

--- Keep/Common/Extensions/Publishers+Extension.swift
+++ Keep/Common/Extensions/Publishers+Extension.swift
@@ -38,26 +38,35 @@ extension Publisher {
 
 //https://stackoverflow.com/a/65884335/10648994
 extension Publisher {
-  func withLatestFrom<P>(
-    _ other: P
-  ) -> AnyPublisher<(Self.Output, P.Output), Failure> where P: Publisher, Self.Failure == P.Failure {
-    let other = other
-      // Note: Do not use `.map(Optional.some)` and `.prepend(nil)`.
-      // There is a bug in iOS versions prior 14.5 in `.combineLatest`. If P.Output itself is Optional.
-      // In this case prepended `Optional.some(nil)` will become just `nil` after `combineLatest`.
-      .map { (value: $0, ()) }
-      .prepend((value: nil, ()))
+    func withLatestFrom<P>(
+        _ other: P
+    ) -> AnyPublisher<(Self.Output, P.Output), Failure> where P: Publisher, Self.Failure == P.Failure {
+        let other = other
+        // Note: Do not use `.map(Optional.some)` and `.prepend(nil)`.
+        // There is a bug in iOS versions prior 14.5 in `.combineLatest`. If P.Output itself is Optional.
+        // In this case prepended `Optional.some(nil)` will become just `nil` after `combineLatest`.
+            .map { (value: $0, ()) }
+            .prepend((value: nil, ()))
+        
+        return map { (value: $0, token: UUID()) }
+            .combineLatest(other)
+            .removeDuplicates(by: { (old, new) in
+                let lhs = old.0, rhs = new.0
+                return lhs.token == rhs.token
+            })
+            .map { ($0.value, $1.value) }
+            .compactMap { (left, right) in
+                right.map { (left, $0) }
+            }
+            .eraseToAnyPublisher()
+    }
+}
 
-    return map { (value: $0, token: UUID()) }
-      .combineLatest(other)
-      .removeDuplicates(by: { (old, new) in
-        let lhs = old.0, rhs = new.0
-        return lhs.token == rhs.token
-      })
-      .map { ($0.value, $1.value) }
-      .compactMap { (left, right) in
-        right.map { (left, $0) }
-      }
-      .eraseToAnyPublisher()
-  }
+//https://forums.swift.org/t/does-assign-to-produce-memory-leaks/29546/9
+extension Publisher where Self.Failure == Never {
+    public func assignNoRetain<Root>(to keyPath: ReferenceWritableKeyPath<Root, Self.Output>, on object: Root) -> AnyCancellable where Root: AnyObject {
+        sink { [weak object] (value) in
+            object?[keyPath: keyPath] = value
+        }
+    }
 }
