032809a9891c3e42456f06242ae1a0ff185038ac
Myung Hoon So <joa214@hotmail.com>
2024-03-30 12:42:16 +0000

    view extension

--- Keep/Common/Extensions/View+Extension.swift
+++ Keep/Common/Extensions/View+Extension.swift
@@ -19,3 +19,19 @@ extension View {
     }
 }
 
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func modify(@ViewBuilder _ transform: (Self) -> (some View)?) -> some View {
+        if let view = transform(self), !(view is EmptyView) {
+            view
+        } else {
+            self
+        }
+    }
+}
