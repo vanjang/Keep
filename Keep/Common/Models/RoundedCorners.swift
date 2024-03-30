8ff8737c06eaa8df9119e918d490e27f324cb791
Myung Hoon So <joa214@hotmail.com>
2024-03-30 12:33:08 +0000

    custom bottom sliding sheet

--- Keep/Common/Models/RoundedCorners.swift
+++ Keep/Common/Models/RoundedCorners.swift
@@ -5,4 +5,15 @@
 //  Created by myung hoon on 29/03/2024.
 //
 
-import Foundation
+import UIKit
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
