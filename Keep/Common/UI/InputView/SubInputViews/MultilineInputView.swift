4fe265da5729a2cc87b4156b6c9ea899921840f1
Myung Hoon So <joa214@hotmail.com>
2024-04-06 23:21:07 +0100

    multiline input view ui bug fixed

--- Keep/Common/UI/InputView/SubInputViews/MultilineInputView.swift
+++ Keep/Common/UI/InputView/SubInputViews/MultilineInputView.swift
@@ -18,7 +18,7 @@ struct MultilineInputView: InputViewType, View {
     let canPresentMenu: Bool
     @Binding var refresh: Bool
     var inputText: (String) -> Void
-
+    
     //MARK: - States
     @State var text: String = ""
     @State private var isEditing = false
@@ -29,66 +29,64 @@ struct MultilineInputView: InputViewType, View {
     
     var body: some View {
         HStack {
-            ZStack(alignment: .trailing) {
-                ZStack(alignment: .topLeading) {
-                    VStack {
-                        TextEditor(text: $text)
-                            .foregroundColor(disabled ? Color(uiColor: .darkGray) : .black)
-                            .frame(height: isEditing || !text.isEmpty ? 150 : 50)
-                            .padding(.horizontal)
-                            .padding(.top, 8)
-                            .disabled(disabled)
-                            .background(.white)
-                            .cornerRadius(16)
-                            .onChange(of: text) { newText in
-                                if newText.count > characterLimit {
-                                    text = String(newText.prefix(characterLimit))
+            ZStack(alignment: .topLeading) {
+                VStack {
+                    TextEditor(text: $text)
+                        .foregroundColor(disabled ? Color(uiColor: .darkGray) : .black)
+                        .frame(height: isEditing || !text.isEmpty ? 150 : 50)
+                        .padding(.horizontal)
+                        .padding(.top, 8)
+                        .disabled(disabled)
+                        .background(.white)
+                        .cornerRadius(16)
+                        .onChange(of: text) { newText in
+                            if newText.count > characterLimit {
+                                text = String(newText.prefix(characterLimit))
+                            }
+                        }
+                        .onReceive(Publishers.keyboardHeight) { height in
+                            let keyboardUp = height > 0
+                            didBeginEditing = keyboardUp
+                            isEditing = keyboardUp && didBeginEditing
+                        }
+                        .contextMenu {
+                            if canPresentMenu {
+                                Button {
+                                    UIPasteboard.general.string = text
+                                } label: {
+                                    Label("Copy", systemImage: "doc.on.doc")
                                 }
+                            } else {
+                                EmptyView()
                             }
-                            .onReceive(Publishers.keyboardHeight) { height in
-                                let keyboardUp = height > 0
-                                didBeginEditing = keyboardUp
-                                isEditing = keyboardUp && didBeginEditing
-                            }
-                            .contextMenu {
-                                if canPresentMenu {
-                                    Button {
-                                        UIPasteboard.general.string = text
-                                    } label: {
-                                        Label("Copy", systemImage: "doc.on.doc")
-                                    }
-                                } else {
-                                    EmptyView()
-                                }
-                            }
-                                
-                                if !disabled {
-                                Text("\(characterLimit - text.count)/\(characterLimit)")
-                                    .foregroundColor(text.count > characterLimit ? .red : .secondary)
-                                    .font(.system(size: 12))
-                                    .frame(maxWidth: .infinity, alignment: .trailing)
-                                    .padding(.trailing, 8)
-                                    .padding(.top, 2)
-                            }
-                        
+                        }
+                    
+                    if !disabled {
+                        Text("\(characterLimit - text.count)/\(characterLimit)")
+                            .foregroundColor(text.count > characterLimit ? .red : .secondary)
+                            .font(.system(size: 12))
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .padding(.trailing, 8)
+                            .padding(.top, 2)
                     }
                     
-                    Text(placeholder)
-                        .foregroundColor(Color(uiColor: .placeholderText))
-                        .padding(.horizontal)
-                        .padding(.top, 16)
-                        .padding(.leading, 4)
-                        .opacity(text.isEmpty ? 1 : 0)
-                        .allowsHitTesting(false)
                 }
                 
-                if canEdit {
-                    Image(systemName: "chevron.right")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 17, height: 17)
-                        .foregroundColor(Color(uiColor: .systemBlue))
-                }                
+                Text(placeholder)
+                    .foregroundColor(Color(uiColor: .placeholderText))
+                    .padding(.horizontal)
+                    .padding(.top, 16)
+                    .padding(.leading, 4)
+                    .opacity(text.isEmpty ? 1 : 0)
+                    .allowsHitTesting(false)
+            }
+            
+            if canEdit {
+                Image(systemName: "chevron.right")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 17, height: 17)
+                    .foregroundColor(Color(uiColor: .systemBlue))
             }
         }
         .onChange(of: text) { newValue in
